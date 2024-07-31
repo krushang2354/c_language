ddb21a4473bce83eee20e95413d9143dec9b5e07
krushang2354 <krushanglimbasiya@gmail.com>
2024-07-31 13:47:56 +0530

    Add files via upload

---
+++ q 3.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+main(){
+
+	//int fa,sa;
+
+	//printf("First angle:");
+	//scanf("%d",&fa);
+
+	//printf("Second angle:");
+	//scanf("%d",&sa);
+
+	//int total=180-(fa+sa);
+	//printf("Third angle:%d",total);
+
+	int fa,sa;
+
+	printf("First angle :");
+	scanf("%d",&fa);
+
+	printf("Second angle :");
+	scanf("%d",&sa);
+
+	int total=180-(fa+sa);
+	printf("Third angle :%d",total);
+
+
+}
